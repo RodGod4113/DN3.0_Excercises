c683d91c277f55917ea79552bc8eb8e694f4d434
RodGod4113 <nxhjsk@gmail.com>
2024-08-29 10:57:08 +0530

    Add files via upload

---
+++ Week5/1.BookStoreAPI-Ex8-Ex9/target/generated-sources/annotations/com/swetha/BookStoreAPI/dto/BookMapperImpl.java
@@ -0,0 +1,46 @@
+package com.swetha.BookStoreAPI.dto;
+
+import com.swetha.BookStoreAPI.model.Book;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-08-27T20:01:29+0530",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.39.0.v20240725-1906, environment: Java 17.0.11 (Eclipse Adoptium)"
+)
+public class BookMapperImpl implements BookMapper {
+
+    @Override
+    public BookDTO bookToBookDTO(Book book) {
+        if ( book == null ) {
+            return null;
+        }
+
+        BookDTO bookDTO = new BookDTO();
+
+        bookDTO.setId( book.getId() );
+        bookDTO.setTitle( book.getTitle() );
+        bookDTO.setAuthor( book.getAuthor() );
+        bookDTO.setPrice( book.getPrice() );
+        bookDTO.setIsbn( book.getIsbn() );
+
+        return bookDTO;
+    }
+
+    @Override
+    public Book bookDTOToBook(BookDTO bookDTO) {
+        if ( bookDTO == null ) {
+            return null;
+        }
+
+        Book book = new Book();
+
+        book.setId( bookDTO.getId() );
+        book.setTitle( bookDTO.getTitle() );
+        book.setAuthor( bookDTO.getAuthor() );
+        book.setPrice( bookDTO.getPrice() );
+        book.setIsbn( bookDTO.getIsbn() );
+
+        return book;
+    }
+}
---
+++ Week5/1.BookStoreAPI-Ex8-Ex9/target/generated-sources/annotations/com/swetha/BookStoreAPI/dto/CustomerMapperImpl.java
@@ -0,0 +1,44 @@
+package com.swetha.BookStoreAPI.dto;
+
+import com.swetha.BookStoreAPI.model.Customer;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-08-27T20:01:29+0530",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.39.0.v20240725-1906, environment: Java 17.0.11 (Eclipse Adoptium)"
+)
+public class CustomerMapperImpl implements CustomerMapper {
+
+    @Override
+    public CustomerDTO customerToCustomerDTO(Customer customer) {
+        if ( customer == null ) {
+            return null;
+        }
+
+        CustomerDTO customerDTO = new CustomerDTO();
+
+        customerDTO.setId( customer.getId() );
+        customerDTO.setName( customer.getName() );
+        customerDTO.setEmail( customer.getEmail() );
+        customerDTO.setAddress( customer.getAddress() );
+
+        return customerDTO;
+    }
+
+    @Override
+    public Customer customerDTOToCustomer(CustomerDTO customerDTO) {
+        if ( customerDTO == null ) {
+            return null;
+        }
+
+        Customer customer = new Customer();
+
+        customer.setId( customerDTO.getId() );
+        customer.setName( customerDTO.getName() );
+        customer.setEmail( customerDTO.getEmail() );
+        customer.setAddress( customerDTO.getAddress() );
+
+        return customer;
+    }
+}
